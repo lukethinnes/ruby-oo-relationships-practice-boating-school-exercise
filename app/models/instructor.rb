88bc5be3745eb5d84f244804616a143c499401e8
Luke Thinnes <lukethinnes@yahoo.com>
2020-12-17 15:46:56 -0700

    commit tree 773a0dbd4a112c84fba88339064f0059697cac8a

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,27 @@
 class Instructor
 
+    def initialize name 
+
+    end
+
+    def self.all
+        @@all
+    end
+
+    def passed_students
+
+    end
+
+    def pass_students
+
+    end
+
+    def fail_student
+
+    end
+
+    def all_students
+
+    end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,28 @@
 class Student
 
+    @@all = []
+
+    def initialize first_name
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def all_instructor
+
+    def self.find_student
+
+    end
+
+    def grade_percentage
+
+    end
+
 end
